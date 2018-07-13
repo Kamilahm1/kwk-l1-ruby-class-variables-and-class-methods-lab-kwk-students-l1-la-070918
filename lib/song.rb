5ff8bc5182ffb8037723b5ad26d369077e53914c
Kamilahm1 <kamilahmorgan1@gmail.com>
2018-07-13 20:30:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,14 @@
+class Song
+ def self.@@Happy.
+   
+   _count
+  attr_accessor:name, :artist, :genre
+  @@count=0
+   @@artist=[ ]
+   @genre=[ ]
+end
+end
+@@Song_count + =1
 
+
+def intialize(name, artist, genre)
